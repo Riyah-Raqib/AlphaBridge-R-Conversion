92014cae3b7c35bd4188c4f5b4034c39144dc1cf
Riyah <104932077+Riyah-Raqib@users.noreply.github.com>
2025-03-06 15:24:44 +0000

    Update and rename conservation_score.py to conservation_score.r

---
+++ src/module/conservation_score.r
@@ -0,0 +1,393 @@
+# -*- coding: utf-8 -*-
+# 
+# Created on Wed Feb 7 16:53:05 2024
+# 
+# @author: Dan_salv
+
+# Load required libraries
+library(ini)
+library(R6)
+# We use base R functions for math and string manipulation.
+
+# Read configuration file
+module_dir <- getwd()  # Using current working directory as module directory
+config <- read.ini(file.path(module_dir, "config.ini"))
+
+MATRIX_DIR <- config$DEFAULT$MATRIX_DIR
+BLOSUM <- config$FILEPATH$BLOSUM
+
+matrix_filepath <- file.path(MATRIX_DIR, BLOSUM)
+
+PSEUDOCOUNT <- 0.0000001
+
+amino_acids <- c('A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', '-')
+iupac_alphabet <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z", "X", "*", "-") 
+
+# dictionary to map from amino acid to its row/column in a similarity matrix
+aa_to_index <- list()
+for(i in seq_along(amino_acids)){
+    aa_to_index[[ amino_acids[i] ]] <- i
+}
+
+blosum_background_distr <- c(0.078, 0.051, 0.041, 0.052, 0.024, 0.034, 0.059, 0.083, 0.025, 0.062, 0.092, 0.056, 0.024, 0.044, 0.043, 0.059, 0.055, 0.014, 0.034, 0.072)
+
+
+# BLOSUM62 background distribution
+blosum_background_distr <- c(0.078, 0.051, 0.041, 0.052, 0.024, 0.034, 0.059, 0.083, 0.025, 0.062, 0.092, 0.056, 0.024, 0.044, 0.043, 0.059, 0.055, 0.014, 0.034, 0.072)
+
+CONSERVATION_SCORE <- R6Class("CONSERVATION_SCORE",
+  public = list(
+    msa = NULL,
+    s_matrix_file = NULL,
+    scoring_function = NULL,
+    window_size = NULL,
+    use_gap_penalty = NULL,
+    win_lam = NULL,
+    gap_cutoff = NULL,
+    bg_distribution = NULL,
+    
+    initialize = function(msa,
+                          s_matrix_file = matrix_filepath,
+                          scoring_function = 'js_divergence', 
+                          window_size = 3,
+                          use_gap_penalty = 1,
+                          win_lam = 0.5 ,
+                          gap_cutoff = 0.3 ,
+                          bg_distribution = blosum_background_distr) {
+        self$msa <- msa
+        self$s_matrix_file <- s_matrix_file
+        self$scoring_function <- scoring_function
+        self$window_size <- window_size
+        self$use_gap_penalty <- use_gap_penalty
+        self$win_lam <- win_lam
+        self$gap_cutoff <- gap_cutoff
+        self$bg_distribution <- bg_distribution
+    },
+          
+    calculate_score = function() {
+            s_matrix_file <- self$s_matrix_file
+            msa <- self$msa
+            gap_cutoff <- self$gap_cutoff
+            bg_distribution <- self$bg_distribution
+            use_gap_penalty <- self$use_gap_penalty
+            names <- msa$descriptions
+            alignment <- modify_alingment(msa$sequences)
+            scoring_function <- self$scoring_function
+            window_size <- self$window_size
+            win_lam <- self$win_lam
+            
+            if(scoring_function == 'js_divergence'){
+                scoring <- js_divergence
+            } else if(scoring_function == 'relative_entropy'){
+                scoring <- relative_entropy
+            } else {
+                stop('Wrong scoring function')
+            }
+            
+            scores <- c()
+            
+            seq_weights <- calculate_sequence_weights(alignment)
+            s_matrix <- read_scoring_matrix(s_matrix_file)
+            
+            for(i in 1:nchar(alignment[[1]])){
+                col <- get_column(i, alignment)
+                if(length(col) == length(alignment)){
+                    scores[length(scores) + 1] <- scoring(col, s_matrix, bg_distribution, seq_weights, use_gap_penalty)
+                } else {
+                    stop('alginment does not have proper length')
+                }
+            }
+            
+            if(window_size > 0){
+                scores <- window_score(scores, window_size, win_lam)
+            }
+            
+            return(scores)
+    }
+  )
+)
+
+modify_alingment <- function(alignment){
+    new_alignment <- c()
+    for(seq in alignment){
+        # Iterate through each character in the sequence
+        chars <- strsplit(seq, "")[[1]]
+        for(aa in chars){
+            if(!(aa %in% iupac_alphabet)){
+                seq <- gsub(aa, "-", seq, fixed=TRUE)
+            } else if(aa %in% c('B' ,'Z','X', '*')){
+                seq <- gsub("B", "D", seq, fixed=TRUE)
+                seq <- gsub("Z", "Q", seq, fixed=TRUE)
+                seq <- gsub("X", "-", seq, fixed=TRUE)
+                seq <- gsub("*", "-", seq, fixed=TRUE)
+            }
+        }
+        new_alignment <- c(new_alignment, seq)
+    }
+    return(new_alignment)
+}
+        
+read_scoring_matrix <- function(sm_file){
+    " Read in a scoring matrix from a file, e.g., blosum80.bla, and return it
+    as an array. "
+    aa_index <- 0
+    first_line <- 1
+    row <- c()
+    list_sm <- list() # hold the matrix in list form
+
+    result <- tryCatch({
+        matrix_file <- readLines(sm_file)
+        
+        for(line in matrix_file){
+            if(substr(line, 1, 1) != "#" && first_line == 1){
+                first_line <- 0
+                if(length(amino_acids) == 0){
+                    tokens <- strsplit(line, "\\s+")[[1]]
+                    for(token in tokens){
+                        aa_to_index[[ tolower(token) ]] <- aa_index
+                        amino_acids <<- c(amino_acids, tolower(token))
+                        aa_index <- aa_index + 1
+                    }
+                }
+            } else if(substr(line, 1, 1) != "#" && first_line == 0){
+                if(nchar(line) > 1){
+                    row <- strsplit(line, "\\s+")[[1]]
+                    list_sm[[length(list_sm) + 1]] <- row
+                }
+            }
+        }
+        list_sm
+    }, error = function(e){
+        return(diag(20))
+    })
+    
+    list_sm <- result
+    
+    # if matrix is stored in lower tri form, copy to upper
+    if(length(list_sm[[1]]) < 20){
+        for(i in 1:19){
+            for(j in (i+1):20){
+                list_sm[[i]] <- c(list_sm[[i]], list_sm[[j]][i])
+            }
+        }
+    }
+    
+    for(i in seq_along(list_sm)){
+        list_sm[[i]] <- as.numeric(list_sm[[i]])
+    }
+    
+    return(list_sm)
+}
+
+calculate_sequence_weights <- function(msa){
+    seq_weights <- rep(0, length(msa))
+    for(i in 1:nchar(msa[[1]])){
+            freq_counts <- rep(0, length(amino_acids))
+            for(j in seq_along(msa)){
+                current_char <- substr(msa[[j]], i, i)
+                if(current_char != '-'){
+                    index <- aa_to_index[[ current_char ]]
+                    freq_counts[index] <- freq_counts[index] + 1
+                }
+            }
+            num_observed_types <- 0
+            for(j in seq_along(freq_counts)){
+                if(freq_counts[j] > 0){ num_observed_types <- num_observed_types + 1 }
+            }
+            for(j in seq_along(msa)){
+                current_char <- substr(msa[[j]], i, i)
+                d <- freq_counts[ aa_to_index[[ current_char ]] ] * num_observed_types
+                if(d > 0){
+                    seq_weights[j] <- seq_weights[j] + 1 / d
+                }
+            }
+    }
+    
+    for(w in seq_along(seq_weights)){
+                seq_weights[w] <- seq_weights[w] / nchar(msa[[1]])
+    }
+    
+    return(seq_weights)
+}
+
+get_column <- function(col_num, alignment){
+    "Return the col_num column of alignment as a list."
+    col <- c()
+    for(seq in alignment){
+        if(col_num <= nchar(seq)){
+            col <- c(col, substr(seq, col_num, col_num))
+        }
+    }
+    
+    return(col)
+}
+gap_percentage <- function(col){
+    
+    num_gaps <- 0.0
+
+    for(aa in col){
+        if(aa == '-'){ num_gaps <- num_gaps + 1 }
+    }
+
+    return(num_gaps / length(col))
+}
+
+weighted_freq_count_pseudocount <- function(col, seq_weights, pc_amount){
+    # if the weights do not match, use equal weight
+    if(length(seq_weights) != length(col)){
+        seq_weights <- rep(1.0, length(col))
+    }
+     
+    aa_num <- 1
+    freq_counts <- rep(pc_amount, length(amino_acids)) # in order defined by amino_acids
+
+    for(aa in amino_acids){
+        for(j in seq_along(col)){
+            if(col[j] == aa){
+                freq_counts[aa_num] <- freq_counts[aa_num] + 1 * seq_weights[j]
+            }
+        }
+        aa_num <- aa_num + 1
+    }
+    
+    total <- sum(seq_weights) + length(amino_acids) * pc_amount
+    for(j in seq_along(freq_counts)){
+        freq_counts[j] <- freq_counts[j] / total
+    }
+    
+    return(freq_counts)
+}
+
+weighted_gap_penalty <- function(col, seq_weights){
+    " Calculate the simple gap penalty multiplier for the column. If the 
+    sequences are weighted, the gaps, when penalized, are weighted 
+    accordingly. "
+    
+    # if the weights do not match, use equal weight
+    if(length(seq_weights) != length(col)){
+        seq_weights <- rep(1.0, length(col))
+    }
+    
+    gap_sum <- 0.0
+    for(i in seq_along(col)){
+        if(col[i] == '-'){
+            gap_sum <- gap_sum + seq_weights[i]
+        }
+    }
+    
+    return(1 - (gap_sum / sum(seq_weights)))
+}
+     
+window_score <- function(scores, window_len, lam = 0.5){
+    
+    w_scores <- scores
+    for(i in (window_len + 1):(length(scores) - window_len)){
+        if(scores[i] < 0){
+            next
+        }
+        sum_val <- 0.0
+        num_terms <- 0.0
+        for(j in (i - window_len):(i + window_len)){
+            if(i != j && scores[j] >= 0){
+                num_terms <- num_terms + 1
+                sum_val <- sum_val + scores[j]
+            }
+            if(num_terms > 0){
+                w_scores[i] <- (1 - lam) * (sum_val / num_terms) + lam * scores[i]
+            }
+        }
+    }
+    
+    return(w_scores)
+}
+
+relative_entropy <- function(col, sim_matix, bg_distr, seq_weights, gap_penalty=1){
+        distr <- bg_distr
+        
+        fc <- weighted_freq_count_pseudocount(col, seq_weights, PSEUDOCOUNT)
+    
+        # remove gap count
+        if(length(distr) == 20){
+            new_fc <- fc[1:(length(fc)-1)]
+            s_val <- sum(new_fc)
+            for(i in seq_along(new_fc)){
+                new_fc[i] <- new_fc[i] / s_val
+            }
+            fc <- new_fc
+        }
+        
+        if(length(fc) != length(distr)){
+            return(-1)
+        }
+        
+        d <- 0.0
+        for(i in seq_along(fc)){
+            if(distr[i] != 0.0){
+                d <- d + fc[i] * log(fc[i] / distr[i])
+            }
+        }
+        
+        d <- d / log(length(fc))
+        
+        if(gap_penalty == 1){
+            return(d * weighted_gap_penalty(col, seq_weights))
+        } else {
+            return(d)
+        }
+}
+    
+js_divergence <- function(col, sim_matix, bg_distr, seq_weights, gap_penalty=1){
+    
+    distr <- bg_distr
+    
+    fc <- weighted_freq_count_pseudocount(col, seq_weights, PSEUDOCOUNT)
+    
+    if(length(distr) == 20){
+        new_fc <- fc[1:(length(fc)-1)]
+        s_val <- sum(new_fc)
+        for(i in seq_along(new_fc)){
+            new_fc[i] <- new_fc[i] / s_val
+        }
+        fc <- new_fc
+    }
+    if(length(fc) != length(distr)){
+        d <- -1
+        return(d)
+    }
+   
+    r <- sapply(1:length(fc), function(i) { 0.5 * fc[i] + 0.5 * distr[i] })
+    
+    d <- 0.0
+    for(i in seq_along(fc)){
+        if(r[i] != 0.0){
+            if(fc[i] == 0.0){
+                d <- d + distr[i] * log(distr[i] / r[i], base=2)
+            } else if(distr[i] == 0.0){
+                d <- d + fc[i] * log(fc[i] / r[i], base=2)
+            } else {
+                d <- d + fc[i] * log(fc[i] / r[i], base=2) + distr[i] * log(distr[i] / r[i], base=2)
+            }
+        }
+    }
+                
+    d <- d / 2        
+            
+    if(gap_penalty == 1){
+        return(d * weighted_gap_penalty(col, seq_weights))
+    } else {
+        return(d)
+    }
+}
+
+# The Python code’s UTF-8 encoding and file header have been preserved as R comments at the beginning.
+# Instead of Python’s os and configparser modules, the R translation uses the "ini" package to read the configuration file. The module directory is approximated using the current working directory via getwd(), as R does not have a direct equivalent of file.
+# The numpy library has no direct counterpart in R; native R functions and data types (e.g., diag for identity matrices) are used instead.
+# The dictionary aa_to_index is implemented as an R list mapping each amino acid (as a string) to its 1-indexed position. Although Python uses 0-indexing, R is 1-indexed. This change is necessary for correct indexing in R.
+# The class CONSERVATION_SCORE is implemented using the R6 package to mimic Python classes and methods. All methods and properties are preserved.
+# The msa parameter is assumed to be a list with at least "descriptions" and "sequences" elements, paralleling the Python object attributes.
+# All helper functions (modify_alingment, read_scoring_matrix, calculate_sequence_weights, get_column, gap_percentage, weighted_freq_count_pseudocount, weighted_gap_penalty, window_score, relative_entropy, and js_divergence) were translated line‐by‐line, preserving their logic.
+# The string manipulation functions use R’s gsub and substr to replicate Python’s str.replace and indexing.
+# Error handling using try/except in Python is replicated with tryCatch in R.
+# In the window_score function, the update of the score inside the inner loop is implemented exactly as in the Python code, even though it may lead to multiple reassignments. This preserves the original behavior.
+# All mathematical operations (e.g., logarithm calculations) use R’s log function. For base 2 logarithms in js_divergence, the base parameter is specified.
+# The formatting and comments from the original Python code have been preserved to the fullest extent possible in R.

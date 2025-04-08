ace8e05291204e69dd2fe34c3e5b7bec0a3e4421
Daniel_laptop <Daalsa98@gmail.com>
2025-04-08 14:50:51 +0200

    fixed ribbon_diagram

--- src/module/ribbon_diagram.py
+++ src/module/ribbon_diagram.py
@@ -280,7 +280,7 @@ def get_interface2color(interfaces_list):
         #cmap = distinctipy.get_colors(interface_nr)    
         color_list = [rgb2hex(cmap(i)[:3]) for i in range(cmap.N)]
         reord_color_list = color_list[::2] + color_list[1::2]
-        color_list = [rgb2hex(rgb) for rgb in cmap]
+        #color_list = [rgb2hex(rgb) for rgb in cmap]
         interface2color = {name:reord_color_list[index]  for index,name in enumerate(interfaces_list)}
         #interface2color = {name:color_list[index]  for index,name in enumerate(interfaces_list)}
         return interface2color
